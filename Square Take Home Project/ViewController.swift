ee66fe4485b20724a63adc657b10a095a1d0e76a
lewist13 <lewistpro@gmail.com>
2022-12-12 18:03:31 -0500

    Initial Commit

---
+++ Square Take Home Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Square Take Home Project
+//
+//  Created by TizzyMatic on 12/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
